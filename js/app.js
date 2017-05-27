1dc6eb3c0dd15c9460573bec98812dddaa724472
maniusia <monia.kum@icloud.com>
2017-05-27 16:09:47 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,7 +2,10 @@
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.x = x;
+    this.y = y
+    
+    this.initialx = x;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -14,6 +17,8 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    
+    
 };
 
 // Draw the enemy on the screen, required method for game
